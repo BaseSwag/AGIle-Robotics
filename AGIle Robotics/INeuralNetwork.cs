1b836aba00eacac959fee7d865abaa1b5b57a4b9
Unknown <03l54rd1n3@gmail.com>
2018-01-16 19:50:57 +0100

    Added Interfaces for NeuralNetworks

---
+++ AGIle Robotics/INeuralNetwork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AGIle_Robotics
+{
+    interface INeuralNetwork : INeuralElement
+    {
+        ILayer[] Layers { get; }
+        int InputSize { get; }
+        int OutputSize { get; }
+    }
+}
