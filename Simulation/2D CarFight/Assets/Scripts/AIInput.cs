bc1e218e2b52215cd325440680e76361b7f83ba9
Maximilian Kenfenheuer <maximilian@mkenfenheuer.de>
2018-01-17 23:20:19 +0100

    Degree calculation

---
+++ Simulation/2D CarFight/Assets/Scripts/AIInput.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AIInput : MonoBehaviour {
+
+    [System.Serializable]
+    public class UnityFloatEvent : UnityEvent<float[]> { };
+
+
+    [SerializeField]
+    public UnityFloatEvent OnInput;
+
+
+    float[] inputs = new float[2];
+
+    public void SetAIInputs(float[] inputs)
+    {
+        this.inputs = inputs;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(OnInput != null)
+            OnInput.Invoke(new float[] { inputs[0] / 180, 0.5f });
+        		
+	}
+}
--- Simulation/2D CarFight/Assets/Scripts/CarController.cs
+++ Simulation/2D CarFight/Assets/Scripts/CarController.cs
@@ -4,28 +4,59 @@ using UnityEngine;
 using UnityEngine.Events;
 
 [RequireComponent(typeof(Rigidbody2D))]
-public class CarController : MonoBehaviour {
+public class CarController : MonoBehaviour
+{
+
+    [System.Serializable]
+    public class UnityFloatEvent : UnityEvent<float[]> { };
+
+
+    [SerializeField]
+    public UnityFloatEvent OnSetAIInputs;
 
     float vertical = 0;
     float hortizontal = 0;
 
+    public float lookRotationRelative = 0;
+    public float lookRotationRelativeClamped = 0;
+
+    public Transform target;
+
     Rigidbody2D rigidbody2D;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         rigidbody2D = GetComponent<Rigidbody2D>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
 
-        rigidbody2D.velocity =  rigidbody2D.velocity * 0.90f;
+    // Update is called once per frame
+    void Update()
+    {
+
+        rigidbody2D.velocity = rigidbody2D.velocity * 0.93f;
 
         if (vertical > 0.1 || vertical < -0.1)
-            rigidbody2D.velocity = transform.right * vertical * Time.deltaTime * 750;
+            rigidbody2D.AddForce(transform.right * vertical * Time.deltaTime * 100000, ForceMode2D.Force);
         if (hortizontal > 0.1 || hortizontal < -0.1)
-            transform.rotation *= Quaternion.Euler(new Vector3(0,0, -hortizontal * Time.deltaTime * 360 / 1.2f));
-	}
+            transform.rotation *= Quaternion.Euler(new Vector3(0, 0, -hortizontal * Time.deltaTime * 360 / 1.2f));
+
+        Vector3 difference = target.position - transform.position;
+        float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+
+        lookRotationRelative = rotationZ + this.transform.rotation.eulerAngles.z;
+
+
+        while (lookRotationRelative > 180)
+            lookRotationRelative -= 360;
+       
+
+        lookRotationRelativeClamped = Mathf.Clamp(lookRotationRelative, -45, 45);
+        Debug.DrawLine(transform.position, target.position);
+
+        OnSetAIInputs.Invoke(new float[] { lookRotationRelative, 0 });
+
+    }
 
     public void OnInput(float[] inputs)
     {
