3317425e74ef2870356c04fd70896a37cf0cf89e
Maximilian Kenfenheuer <maximilian@mkenfenheuer.de>
2018-01-17 16:53:46 +0100

    Car Movement

---
+++ Simulation/2D CarFight/Assets/Scripts/CarController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class CarController : MonoBehaviour {
+
+    float vertical = 0;
+    float hortizontal = 0;
+
+    Rigidbody2D rigidbody2D;
+
+	// Use this for initialization
+	void Start () {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        rigidbody2D.velocity =  rigidbody2D.velocity * 0.90f;
+
+        if (vertical > 0.1 || vertical < -0.1)
+            rigidbody2D.velocity = transform.right * vertical * Time.deltaTime * 750;
+        if (hortizontal > 0.1 || hortizontal < -0.1)
+            transform.rotation *= Quaternion.Euler(new Vector3(0,0, -hortizontal * Time.deltaTime * 360 / 1.2f));
+	}
+
+    public void OnInput(float[] inputs)
+    {
+        hortizontal = inputs[0];
+        vertical = inputs[1];
+    }
+}
---
+++ Simulation/2D CarFight/Assets/Scripts/HumanInput.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class HumanInput : MonoBehaviour {
+
+    [System.Serializable]
+    public class UnityFloatEvent : UnityEvent<float[]> { };
+
+
+    [SerializeField]
+    public UnityFloatEvent OnInput;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        OnInput.Invoke(new float[] { Input.GetAxis("Horizontal"), Input.GetAxis("Vertical") });
+	}
+}
