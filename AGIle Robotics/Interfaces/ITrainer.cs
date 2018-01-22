ce11f23b2beb6dd9959eafe05f79044a284b76d9
Marius Mertens <logins@mariusmertens.de>
2018-01-22 19:11:57 +0100

    SuperTuple project

---
+++ AGIle Robotics/Interfaces/ITrainer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AGIle_Robotics.Interfaces
+{
+    public interface ITrainer
+    {
+        double TransitionRatio { get; set; }
+        double RandomRatio { get; set; }
+        double MutationRatio { get; set; }
+        IGeneration CurrentGeneration { get; }
+        Func<INeuralNetwork, INeuralNetwork, Task<Tuple<double, double>>> FitnessFunction { get; set; }
+        Task Initialize(int size, Tuple<int, int> popSize, Tuple<int, int> ports, Tuple<int, int> length, Tuple<int, int> width, Tuple<double, double> weightRange, Func<double, double> activateWith);
+        Task Create();
+        Task Evolve();
+        Task Evaluate();
+        Task Evaluate(Func<INeuralNetwork, INeuralNetwork, Task<Tuple<double, double>>> fitnessFunction);
+    }
+}
