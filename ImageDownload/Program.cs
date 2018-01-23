d3a9c1b53875559ae457c051da98a2891eabb22b
Maximilian Kenfenheuer <maximilian@mkenfenheuer.de>
2018-01-23 19:52:36 +0100

    Max 1500 Bilder

--- ImageDownload/Program.cs
+++ ImageDownload/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Net;
@@ -14,15 +14,18 @@ namespace ImageDownload
         static void Main(string[] args)
         {
             WebClient wc = new WebClient();
-            timer.Interval = 500;
+            timer.Interval = 1000 / 8;
             timer.Tick += (o, e) => {
                 timer.Stop();
-                wc.DownloadFileAsync(new Uri("http://10.0.0.1:8080/?action=snapshot"), "images\image_" + DateTime.Now.Ticks + ".jpg");
+                wc.DownloadFileAsync(new Uri("http://10.0.0.1:8080/?action=snapshot"), "images\\image_" + DateTime.Now.Ticks + ".jpg");
             };
 
             wc.DownloadFileCompleted += (o, e) => {
-                Console.WriteLine("File downloaded");
-                timer.Start();
+                Console.Clear();
+                int pics = Directory.GetFiles("images\\").Length;
+                if ( pics < 15001)
+                    timer.Start();
+                Console.WriteLine("Files downloaded: " + pics);
             };
 
             timer.Start();
