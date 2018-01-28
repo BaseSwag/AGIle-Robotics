b71091a3f000e978e52ef2bc6c8c0db036b0fe70
Unknown <03l54rd1n3@gmail.com>
2018-01-28 13:11:46 +0100

    TestConsole implements new ViewModel

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using AGIle_Robotics;
 using AGIle_Robotics.Extension;
@@ -13,75 +14,64 @@ namespace TestConsole
 {
     class Program
     {
-        const int steps = 50;
-        static WorkPool workPool = new WorkPool(4);
-        static Random random = new Random();
-        static Task[] tasks = new Task[steps];
-
-        static int[] proof = new int[steps];
+        static Trainer Trainer;
+        static Task Task;
 
         static void Main(string[] args)
         {
-            //PerformanceTests.ListPop();
-            Usage.Init().Wait();
-            Usage.EvaluateAndEvolve(5).Wait();
+            Trainer = new Trainer(
+                transitionRatio: 0.5,
+                randomRatio: 0.1,
+                mutationRatio: 0.1);
+
+            Task = Trainer.InitializeAndCreate(
+                size: 10,
+                popSize: (10, 15),
+                ports: (1, 1),
+                length: (5, 15),
+                width: (2, 10),
+                weightRange: (-2.0, 2.0),
+                activateWith: Math.Tanh
+                );
+
+            ReportStatus(Task).Wait();
+
+            Trainer.FitnessFunction = FitnessFunction;
+
+            for (int i = 0; i < 2; i++)
+            {
+                Task = Trainer.EvaluateAndEvolve();
+                ReportStatus(Task).Wait();
+            }
+            ReportStatus(Task, true).Wait();
+
             Console.ReadLine();
         }
 
-        static void TestTrainer()
+        static async Task ReportStatus(Task task, bool anyway = false)
         {
+            while (!task.IsCompleted || anyway)
+            {
+                Console.Clear();
+                Console.WriteLine($"Transition ratio: {Trainer.StatusUpdater.TransitionRatio}");
+                Console.WriteLine($"Random ratio: {Trainer.StatusUpdater.RandomRatio}");
+                Console.WriteLine($"Mutation ratio: {Trainer.StatusUpdater.MutationRatio}");
+                Console.WriteLine($"Population count: {Trainer.StatusUpdater.PopulationCount}");
+                Console.WriteLine($"Network count: {Trainer.StatusUpdater.NetworkCount}");
+                Console.WriteLine($"Level: {Trainer.StatusUpdater.GenerationLevel}");
+                Console.WriteLine($"Activity: {Trainer.StatusUpdater.Activity}");
+                Console.WriteLine($"Best fitness: {Trainer.StatusUpdater.BestFitness}");
+                Console.WriteLine($"Evaluations running: {Trainer.StatusUpdater.EvaluationsRunning}");
+                await Task.Delay(500);
+            }
         }
 
-        static async Task<double> TestSumFitness(INeuralNetwork n)
+        static async Task<STuple<double, double>> FitnessFunction (INeuralNetwork n1, INeuralNetwork n2) 
         {
-            var result = await TestSum(n);
-            return result.Item1;
-        }
-        static async Task<(double, double, double, double)> TestSum(INeuralNetwork n)
-        {
+            await Task.Delay(1000);
             Random r = new Random();
-            var a = r.NextDouble();
-            var b = r.NextDouble();
-            var correct = a + b;
-
-            var result = await n.ActivateAsync(new double[] { a, b });
-            double sum = result[0];
-
-            double fitness = correct - sum;
-            fitness = -Math.Abs(fitness);
-            return (fitness, a, b, sum);
-        }
-
-        static async Task<(double, double)> TestLarger(INeuralNetwork n1, INeuralNetwork n2, TaskCompletionSource<STuple<double, double>> tcs)
-        {
-            var a = await n1.ActivateAsync(new double[] { 1 });
-            var b = await n2.ActivateAsync(new double[] { 1 });
-
-            (double, double) result;
-            if(a[0] < b[0])
-            {
-                result = (-1, 1);
-                tcs.SetResult(result);
-            }
-            else
-            {
-                result = (1, -1);
-                tcs.SetResult(result);
-            }
+            var result = (r.NextDouble(), r.NextDouble());
             return result;
-        }
-
-        static Task<STuple<double, double>> FitnessFunction (INeuralNetwork n1, INeuralNetwork n2) 
-        {
-            TaskCompletionSource<STuple<double, double>> tcs = new TaskCompletionSource<STuple<double, double>>();
-            TestLarger(n1, n2, tcs);
-            return tcs.Task;
-        }
-
-        static async Task TestFitnessFunction(INeuralNetwork n1, INeuralNetwork n2, TaskCompletionSource<STuple<double, double>> tcs)
-        {
-            var results = await Task.WhenAll(new Task<double>[] { TestSumFitness(n1), TestSumFitness(n1) });
-            tcs.SetResult((results[0], results[1]));
         }
     }
 }
