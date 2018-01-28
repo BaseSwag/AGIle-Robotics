0c788265e678216bf2a17b6c360df56a328e0d51
Unknown <03l54rd1n3@gmail.com>
2018-01-28 13:10:34 +0100

    Removing asyncs agains

--- AGIle Robotics/Interfaces/INeuralElement.cs
+++ AGIle Robotics/Interfaces/INeuralElement.cs
@@ -11,8 +11,6 @@ namespace AGIle_Robotics.Interfaces
         double[] Activate(double[] input);
         Task<double[]> ActivateAsync(double[] input);
         INeuralElement CrossOver(INeuralElement e, double p1, double p2);
-        Task<INeuralElement> CrossOverAsync(INeuralElement e, double p1, double p2);
         void Mutate(double ratio);
-        Task MutateAsync(double ratio);
     }
 }
