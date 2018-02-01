7297353ad665d3530304d51e3b2cd21665f8e298
Unknown <03l54rd1n3@gmail.com>
2018-02-01 23:58:54 +0100

    JsonConverters

---
+++ SuperTuple/DoubleTupleJsonConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace SuperTuple
+{
+    public class DoubleTupleJsonConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            var isDoubleValueTuple = objectType == typeof(ValueTuple<double, double>);
+            var isDoubleTuple = objectType == typeof(Tuple<double, double>);
+            var isDoubleSTuple = objectType == typeof(STuple<double, double>);
+            return (isDoubleValueTuple || isDoubleTuple || isDoubleSTuple);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            var deserialized = serializer.Deserialize<STuple<double, double>>(reader);
+            (double, double) valueTuple = deserialized;
+            return valueTuple;
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            (double, double) valueTuple = ((double, double))value;
+            STuple<double, double> stuple = valueTuple;
+            serializer.Serialize(writer, stuple, typeof(STuple<dynamic, dynamic>));
+        }
+    }
+}
