6ec0d747b4932bbbb48cc19d417d2e0a7b768476
Maximilian Kenfenheuer <maximilian@mkenfenheuer.de>
2018-02-09 15:27:37 +0100

    Added Tooggle Rendering Button

---
+++ Simulation/2D CarFight/Assets/Scripts/ToggleRenderingScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleRenderingScript : MonoBehaviour {
+
+    public Camera mainCamera;
+    private void Start()
+    {
+        mainCamera = Camera.main;
+    }
+    public void OnToggleRendering()
+    {
+        mainCamera.enabled = !mainCamera.enabled;
+    }
+}
