8aec2ae88474da2ae335f23a7a022f6c6e1e9e7d
Daniel Struck <da-struck@outlook.de>
2018-04-13 15:47:22 +0200

    Update detection server

--- bot/init.py
+++ bot/init.py
@@ -2,6 +2,8 @@ import sys, time, json, traceback
 from base64 import b64encode
 from cv2 import imencode
 
+
+from enemy import enemy
 from dashboard.factory import DashboardServerFactory
 from dashboard.protocol import DashboardServerProtocol
 from twisted.web.server import Site
@@ -21,38 +23,111 @@ from twisted.internet import reactor, task
 from twisted.python import log
 
 from autobahn.twisted.websocket import listenWS
+#import main
 
 log.startLogging(sys.stdout)
 
+mode = False
+def changeMode(m):
+    global mode
+
+    print('changing mode to ', m)
+    if m == 'stop':
+        control(mode, 0, 0)
+        mode = False
+    else:
+        mode = m
+
+lastX, lastY = 0, 0
+def control (m, x, y):
+    global mode, lastX, lastY
+    if m == mode:
+        Control.joystick(x, y)
+        lastX, lastY = x, y
+
 factory = DashboardServerFactory(u"ws://127.0.0.1:9000")
 factory.protocol = DashboardServerProtocol
+factory.control = control
+factory.changeMode = changeMode
 listenWS(factory)
-
-lastX = 0
-lastY = 0
-def control(x, y):
-    global lastX, lastY
-    Control.joystick(x, y)
-    lastX, lastY = x, y
 
 aiFactory = AIServerFactory()
 aiFactory.protocol = AIServerProtocol
 aiFactory.control = control
 
-reactor.listenTCP(8337, aiFactory)
+reactor.listenTCP(1337, aiFactory)
 reactor.listenTCP(8000, site)
 
+#def loop():
+    #try:
+        #frame, target = getNextFrame()
+        #success, buffer = imencode('.jpg', frame)
+        #msg = b64encode(buffer)
+        #factory.broadcast('image', msg)
+    #except:
+        #print(traceback.format_exc())
+        #pass
+
+def sendImage(image):
+    success, buffer = imencode('.jpg', image)
+    if success:
+        factory.broadcastImage(buffer.tobytes())
+
+
+def sendStatus(status):
+    factory.broadcastStatus(status)
+
+def getStatus(target):
+    global lastX, lastY
+    target = getTarget(target)
+    return {
+        'target': target, 
+        'lastX': lastX, 
+        'lastY': lastY, 
+        'front': Sensor.getFront(), 
+        'back': Sensor.getBack()
+    }
+
+def getTarget(target):
+    if target == False:
+        return -1
+    x1, y1, x2, y2 = target
+    return ((x1 + x2) / 640.0)
+    #return ((x1 + x2) / 640.0).item()
+
+def activateAI(status):
+    info = [
+        str(status['target']),
+        '0', '0', '0',
+        str(status['front']),
+        str(status['back'])
+    ]
+    msg = ' '.join(info)
+    aiFactory.broadcast(msg)
+
+
+def activateStrategy(status):
+    print('activate', mode, status)
+    if mode == 'ai':
+        activateAI(status)
+    elif mode == 'enemy':
+        enemy(status['target'], 1, control)
+
 def loop():
-    try:
-        frame, target = getNextFrame()
-        success, buffer = imencode('.jpg', frame)
-        msg = b64encode(buffer)
-        factory.broadcast('image', msg)
-    except:
-        print(traceback.format_exc())
-        pass
+    start = time.time()
+
+    frame, target = getNextFrame()
+    frameTime = time.time() - start
+    sendImage(frame)
+
+    status = getStatus(target)
+    activateStrategy(status)
+    status['frameTime'] = frameTime
+    status['loopTime'] = time.time() - start
+    sendStatus(status)
 
 main = task.LoopingCall(loop)
-main.start(1)
+main.start(0)
 
 reactor.run()
+
